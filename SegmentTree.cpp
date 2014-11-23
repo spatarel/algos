f2822c940c30fb23bd2eb428ae395b5d49ec42ad
spatarel <spatarel1@yahoo.com>
2014-11-23 03:40:01 +0200

    Improved the API for the SegmentTree class.

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,95 +1,68 @@
-#include <cstdio>
+const int MOD = 1000000007;
 
 class UpdateType {
-    long long val;
+    long long value;
 
 public:
-    UpdateType(const long long arg) {
-        this->val = arg;
+    UpdateType(long long value = 0) {
+        this->value = value;
     }
 
-    UpdateType& operator = (const long long arg) {
-        this->val = arg;
+    UpdateType& operator = (long long value) {
+        this->value = value;
         return *this;
     }
 
-    UpdateType& operator += (const UpdateType arg) {
-        this->val += arg.val;
+    UpdateType& operator += (const UpdateType &arg) {
+        this->value += arg.value;
+        this->value %= MOD;
         return *this;
     }
 
-    long long get_val() const {
-        return this->val;
+    operator long long() const {
+        return this->value;
     }
 };
 
 class QueryType {
-    long long n;
-    long long sum;
-    long long fsus;
-    long long fjos;
-    long long f;
+    long long length;
+	long long value;
 
 public:
-    QueryType(const long long n) {
-        this->n    = n;
-        this->sum  = 0;
-        this->fsus = 0;
-        this->fjos = 0;
-        this->f    = 0;
+    QueryType(long long length, long long value = 0) {
+        this->length = length;
+    	this->value = value;
     }
 
-    QueryType& operator += (const UpdateType arg) {
-        this->sum  += arg.get_val() * this->n;
-        this->fsus += arg.get_val() * (this->n * (this->n + 1) / 2);
-        this->fjos += arg.get_val() * (this->n * (this->n + 1) / 2);
-        this->f    += arg.get_val() * (this->n * (this->n + 1) * (this->n + 2) / 6);
+    QueryType& operator += (const UpdateType &arg) {
+        this->value += arg;
+        this->value %= MOD;
 
         return *this;
     }
 
-    QueryType operator + (const UpdateType arg) {
-        QueryType nod(this->n);
-
-        nod.sum  = this->sum  + arg.get_val() * this->n;
-        nod.fsus = this->fsus + arg.get_val() * (this->n * (this->n + 1) / 2);
-        nod.fjos = this->fjos + arg.get_val() * (this->n * (this->n + 1) / 2);
-        nod.f    = this->f    + arg.get_val() * (this->n * (this->n + 1) * (this->n + 2) / 6);
-
-        return nod;
+    QueryType operator + (const UpdateType &arg) const {
+        return QueryType(this->length,
+        		(this->value + arg) % MOD);
     }
 
-    QueryType operator * (const QueryType arg) {
-        QueryType nod(this->n + arg.n);
-
-        nod.sum  = this->sum + arg.sum;
-        nod.fsus = this->fsus + arg.fsus + this->n * arg.sum;
-        nod.fjos = this->fjos + arg.fjos + arg.n * this->sum;
-        nod.f    = this->f + arg.f + arg.n * this->fsus + this->n * arg.fjos;
-
-        return nod;
+    QueryType operator * (const QueryType &arg) const {
+        return QueryType(this->length + arg.length,
+        		(this->value + arg.value) % MOD);
     }
 
-    QueryType operator + (const QueryType arg) {
-        //assert(this->n == arg.n);
-
-        QueryType nod(this->n);
-
-        nod.sum  = this->sum  + arg.sum;
-        nod.fsus = this->fsus + arg.fsus;
-        nod.fjos = this->fjos + arg.fjos;
-        nod.f    = this->f    + arg.f;
-
-        return nod;
+    QueryType operator + (const QueryType &arg) const {
+        return QueryType(this->length,
+        		(this->value + arg.value) % MOD);
     }
 
-    double get_f() const {
-        return (double)this->f / (this->n * (this->n + 1) / 2);
+    operator long long() const {
+        return this->value;
     }
 };
 
 
-template<class UpdateType, class QueryType>
+template<class UpdateType = UpdateType, class QueryType = QueryType>
 class SegmentTree {
 private:
     int x1;
@@ -99,7 +72,7 @@ private:
     bool* updated;
 
     void real_init(int pos, int start, int stop) {
-        this->tree[pos] = new UpdateType(0);
+        this->tree[pos] = new UpdateType;
         this->comb[pos] = new QueryType(stop - start + 1);
         if (start == stop) {
             return;
@@ -112,20 +85,18 @@ private:
     void real_update_interval(int pos, int start, int stop, int x1, int x2, UpdateType u) {
         if (start == x1 && stop == x2) {
             *this->tree[pos] += u;
-            return;
+        } else {
+			int med = (start + stop) / 2;
+			if (x2 <= med) { // we go only left
+				this->real_update_interval(2 * pos, start, med, x1, x2, u);
+			} else if (med < x1) { // we go only right
+				this->real_update_interval(2 * pos + 1, med + 1, stop, x1, x2, u);
+			} else { // we go both ways
+				this->real_update_interval(2 * pos, start, med, x1, med, u);
+				this->real_update_interval(2 * pos + 1, med + 1, stop, med + 1, x2, u);
+			}
+	        this->updated[pos] = false;
         }
-        int med = (start + stop) / 2;
-        if (x2 <= med) { // we go only left
-            this->real_update_interval(2 * pos, start, med, x1, x2, u);
-        } else if (med < x1) { // we go only right
-            this->real_update_interval(2 * pos + 1, med + 1, stop, x1, x2, u);
-        } else { // we go both ways
-            this->real_update_interval(2 * pos, start, med, x1, med, u);
-            this->real_update_interval(2 * pos + 1, med + 1, stop, med + 1, x2, u);
-        }
-        this->updated[pos] = false;
-    //    *comb[pos] = (*comb[2 * pos] + *tree[2 * pos]) *
-    //            (*comb[2 * pos + 1] + *tree[2 * pos + 1]);
     }
 
     QueryType real_lazy_query(int pos) {
@@ -142,7 +113,6 @@ private:
             return this->real_lazy_query(pos) + *this->tree[pos];
         }
         int med = (start + stop) / 2;
-        // ASSERT
         if (x2 <= med) { // we go only left
             return this->real_query_interval(2 * pos, start, med, x1, x2) +
                     *this->tree[pos];
@@ -180,33 +150,15 @@ public:
         this->real_update_interval(1, this->x1, this->x2, x1, x2, u);
     }
 
+    void update_value(int x, UpdateType u) {
+    	this->real_update_interval(1, this->x1, this->x2, x, x, u);
+    }
+
     QueryType query_interval(int x1, int x2) {
         return this->real_query_interval(1, this->x1, this->x2, x1, x2);
     }
-};
 
-int main(void) {
-    int i;
-    char Q[20];
-    int A, B, D;
-
-    int N, M;
-
-    // citirea datelor
-    scanf("%d %d", &N, &M);
-    --N;
-    SegmentTree<UpdateType, QueryType> *ST = new SegmentTree<UpdateType, QueryType>(1, N);
-    for (i = 0; i < M; ++i) {
-        scanf("\n%s", Q);
-        if (Q[0] == 'c') {
-            scanf("%d %d %d", &A, &B, &D);
-            ST->update_interval(A, B - 1, (UpdateType)D);
-        } else {
-            scanf("%d %d", &A, &B);
-            printf("%.8lf\n", ST->query_interval(A, B - 1).get_f());
-        }
+    QueryType query_value(int x) {
+        return this->real_query_interval(1, this->x1, this->x2, x, x);
     }
-
-    return 0;
-}
-
+};
