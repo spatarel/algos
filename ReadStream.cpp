d29305867ee89a31a0563eb0689bfc77974e1df2
spatarel <spatarel1@yahoo.com>
2017-01-02 22:09:08 +0200

    Improved the ReadStream.

--- ReadStream.cpp
+++ ReadStream.cpp
@@ -1,60 +1,53 @@
 #include <cstdio>
+#include <cctype>
 
 template<int SIZE>
 class ReadStream {
 private:
-    FILE* stream;
+  FILE* stream;
 
-    int pos;
-    char buffer[SIZE + 1];
+  char buffer[SIZE];
+  char* begin;
+  char* end;
+
+  void readBuffer() {
+    this->begin = this->buffer;
+    this->end = this->begin + fread(this->buffer, sizeof(char), SIZE, this->stream);
+  }
+
+  char currentChar() {
+    return *this->begin;
+  }
+
+  void advance() {
+    this->begin++;
+    if (this->begin == this->end)
+      this->readBuffer();
+  }
 
 public:
-    ReadStream(FILE* stream) {
-        this->stream = stream;
-        this->pos = SIZE;
+  ReadStream(FILE* stream = stdin) {
+    this->stream = stream;
+    this->begin = NULL;
+    this->end = NULL;
+    this->readBuffer();
+  }
+
+  int nextInt() {
+    while (isspace(this->currentChar()))
+      this->advance();
+    int answer = 0;
+    while (isdigit(this->currentChar())) {
+      answer *= 10;
+      answer += this->currentChar() - '0';
+      this->advance();
     }
+    return answer;
+  }
 
-    int nextInt() {
-        int answer = 0;
-        int readSize;
-
-        while (true) {
-            if (this->pos == SIZE) {
-                readSize = fread(this->buffer, sizeof(char), SIZE, this->stream);
-                this->buffer[readSize] = 0;
-                this->pos = 0;
-            }
-            if ('0' <= this->buffer[this->pos] && this->buffer[this->pos] <= '9') {
-                break;
-            } else {
-                ++this->pos;
-            }
-        }
-        while (true) {
-            if (this->pos == SIZE) {
-                readSize = fread(this->buffer, sizeof(char), SIZE, this->stream);
-                this->buffer[readSize] = 0;
-                this->pos = 0;
-            }
-            if (!('0' <= this->buffer[this->pos] && this->buffer[this->pos] <= '9')) {
-                break;
-            } else {
-                answer *= 10;
-                answer += this->buffer[this->pos] - '0';
-                ++this->pos;
-            }
-        }
-
-        return answer;
-    }
-
-    char nextChar() {
-        int readSize;
-		if (this->pos == SIZE) {
-			readSize = fread(this->buffer, sizeof(char), SIZE, this->stream);
-			this->buffer[readSize] = 0;
-			this->pos = 0;
-		}
-		return this->buffer[this->pos++];
-    }
+  char nextChar() {
+    char answer = this->currentChar();
+    this->advance();
+    return answer;
+  }
 };
